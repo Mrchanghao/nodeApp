70fd73bfaf6fe625aaee670bd3d2da7489c73251
이창호 <lnl2048@gmail.com>
2017-11-29 18:36:37 +0900

    add function 완료
    추가적인 리팩토링 필요

---
+++ README.md
@@ -0,0 +1,5 @@
+# node API를 이용한 어플리케이션 연습(udemy 코스) 
+-
+* 노드의 기본 동작 원리와 API 작동 원리 연습 
+* node third party module 활용 
+
---
+++ notes.js
@@ -0,0 +1,68 @@
+console.log('Starting notes.js');
+
+const fs = require('fs');
+
+const addNote = (title, body) => {
+  let notes = [];
+  let note = {
+    title,
+    body
+  };
+  try {
+    const notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+  } catch(err) {
+
+  }
+  
+const duplicateNotes = notes.filter((note) => {
+  return note.title === title;
+});
+
+if (duplicateNotes.length === 0) {
+  notes.push(note);
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+ }
+};
+// const fs = require('fs');
+
+// var addNote = (title, body) => {
+//   var notes = [];
+//   var note = {
+//     title,
+//     body
+//   };
+
+//   try {
+//     var notesString = fs.readFileSync('notes-data.json');
+//     notes = JSON.parse(notesString);
+//   } catch (e) {
+
+//   }
+
+//   var duplicateNotes = notes.filter((note) => note.title === title);
+
+//   if (duplicateNotes.length === 0) {
+//     notes.push(note);
+//     fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+//   }
+// };
+
+// var getAll = () => {
+//   console.log('Getting all notes');
+// };
+
+// var getNote = (title) => {
+//   console.log('Getting note', title);
+// };
+
+// var removeNote = (title) => {
+//   console.log('Removing note', title);
+// };
+
+module.exports = {
+  addNote,
+  // getAll,
+  // getNote,
+  // removeNote
+};
